f090f2527a8f060e7234cd6a27ea1511395fa1fc
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2024-09-18 05:13:48 +0100

    listen on 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use jsonrpsee::server::ServerBuilder;
 use eyre::Result;
-use revm_passthrough_proxy::rpc::{PassthroughProxy, PassthroughApiServer};
+use jsonrpsee::server::ServerBuilder;
+use revm_passthrough_proxy::rpc::{PassthroughApiServer, PassthroughProxy};
 use std::env;
 
 #[tokio::main]
@@ -8,18 +8,19 @@ async fn main() -> Result<()> {
     // Replace with your actual endpoint URL
     let endpoint = env::var("RPC").expect("env var RPC must be set");
 
+    let port = env::var("PORT").expect("env var PORT must be set");
+
     // Initialize the PassthroughProxy
     let proxy = PassthroughProxy::init(&endpoint)?;
 
-    // Build the server on the desired address and port
     let server = ServerBuilder::default()
-        .build("127.0.0.1:8545")
+        .build(format!("0.0.0.0:{}", port))
         .await?;
 
     // Start the server with your RPC methods
     let handle = server.start(proxy.into_rpc());
 
-    println!("Server started at 127.0.0.1:8545");
+    println!("Server started at 0.0.0.0:{}", port);
 
     // Keep the server running indefinitely
     handle.stopped().await;
